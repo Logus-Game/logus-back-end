053be904220484caed9ab029685512d28d406871
Barret0JG <jg.alvesbarreto.c@gmail.com>
2024-07-15 22:26:14 -0300

    jwt no backend

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import mysql.connector
 from dotenv import load_dotenv
 import os
@@ -14,6 +15,8 @@ db_connection = mysql.connector.connect(
 )
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
+jwt = JWTManager(app)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -27,9 +30,26 @@ def login():
     cursor.close()
 
     if user:
+        access_token = create_access_token(identity=user['id'])
+        response = make_response(jsonify(access_token=access_token), 200)
+        response.set_cookie('token', access_token, httponly=True, secure=True)
+        print(access_token)
         return jsonify({'message': 'Login successful'}), 200
     else:
         return jsonify({'message': 'Invalid credentials'}), 401
+
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+    
+@app.route('/quests', methods=['POST'])
+def quests():
+    data = request.get_json()
+    id_user = data['id']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
