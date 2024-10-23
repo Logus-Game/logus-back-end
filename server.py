a1e5ec7a04951cf65b3ae1622b065d0e632ec55c
Barret0JG <jg.alvesbarreto.c@gmail.com>
2024-10-22 23:31:26 -0300

    metodo para update de quest feito

--- server.py
+++ server.py
@@ -22,25 +22,24 @@ jwt = JWTManager(app)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     email = data['email']
     password = data['password']
     cursor = db_connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM usuario WHERE email = %s AND senha = %s order by id desc", (email, password))
+    cursor.execute("SELECT * FROM usuario WHERE email = %s AND senha = %s order by id_usuario desc", (email, password))
     user = cursor.fetchone()
     cursor.close()
 
     if user:
-        access_token = create_access_token(identity=user['id'])
-        response = make_response(jsonify(access_token=access_token), 200)
+        access_token = create_access_token(identity=[user['id_usuario'], user['nivel']])
+        response = make_response(jsonify({'access_token':access_token, 'nivel': user['nivel']}), 200)
         response.set_cookie('token', access_token, httponly=True, secure=False, samesite='Lax')
-
-        
         return response
     else:
-        return response
+        return jsonify({'message':'Usuario ou senha incorretos'})
 
 
 @app.route('/protected', methods=['GET'])
@@ -53,9 +52,9 @@ def protected():
 @app.route('/quests', methods=['GET'])
 @jwt_required()
 def quests():
-    id_user = get_jwt_identity()
+    id_user = get_jwt_identity()[0]
     cursor = db_connection.cursor(dictionary=True)
-    cursor.execute(f"select a.*,q.* from usuario u join usuario_has_quest a on u.id = a.id_usuario join quest q on q.id_quest = a.id_quest where id_usuario = {id_user}")
+    cursor.execute(f"select a.*,q.* from usuario u join usuario_has_quest a on u.id_usuario = a.id_usuario join quest q on q.id_quest = a.id_quest where a.id_usuario = {id_user}")
     quests = cursor.fetchall()
     cursor.close()
 
@@ -64,6 +63,54 @@ def quests():
         
     else:
         return jsonify(), 204
+
+@app.route('/quests/status/<int:quest_id>', methods=['PATCH'])
+@jwt_required()
+def updateQuestStatus(quest_id):
+    try:
+        id_user = get_jwt_identity()[0]
+        data = request.json
+        status = data.get('status', None)
+        cursor = db_connection.cursor(dictionary=True)
+        cursor.execute(f'update usuario_has_quest set estado="{status}" where id_quest={quest_id} and id_usuario={id_user}')
+        db_connection.commit()
+
+        return jsonify({"message": "quest updated successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": f"{e}"}), 500
+
+@app.route('/user-data', methods=['GET'])
+@jwt_required()
+def userData():
+    id_user = get_jwt_identity()[0]
+    cursor = db_connection.cursor(dictionary=True)
+    cursor.execute(f"select * from usuario where id_usuario={id_user}")
+    data = cursor.fetchone()
+    cursor.close()
+
+    if data:
+        return jsonify({'message': 'sucesso', 'info': data}), 200
+    else:
+        return jsonify(), 204
+    
+
+@app.route('/players', methods=['GET'])
+@jwt_required()
+def players():
+    nivel = get_jwt_identity()[1]
+    if nivel != 'AA':
+        return jsonify({'message': 'Acesso negado', 'info': ''}), 403
+    cursor = db_connection.cursor(dictionary=True)
+    cursor.execute(f"select * from usuario where nivel != 'AA'")
+    data = cursor.fetchall()
+    cursor.close()
+
+    if data:
+        return jsonify({'message': 'sucesso', 'info': data}), 200
+    else:
+        return jsonify(), 204
+
+
     
 if __name__ == '__main__':
     app.run(debug=True)
